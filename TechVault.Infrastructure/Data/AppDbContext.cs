a3f9d7951662c2cd4bc4fb55b1f8d64733426306
Ali Abooof <aliabooof@outlook.com>
2025-03-03 15:42:14 +0200

    Creating application database context

---
+++ TechVault.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechVault.Core.Entities.Product;
+
+namespace TechVault.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Photo> Photos { get; set; }
+
+
+    }
+}
