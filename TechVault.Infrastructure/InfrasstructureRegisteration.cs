07598cf8fc035956d3efa6d51ab70e60d1cf120e
Ali Abooof <aliabooof@outlook.com>
2025-03-03 16:11:38 +0200

    creating Category Repository

---
+++ TechVault.Infrastructure/InfrasstructureRegisteration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechVault.Core.Interfaces;
+using TechVault.Infrastructure.Repositories;
+
+namespace TechVault.Infrastructure
+{
+    public static class InfrasstructureRegisteration
+    {
+        public static IServiceCollection infrastructureConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            services.AddScoped<ICategoryRepository,CategoryRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ TechVault.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechVault.Core.Entities.Product;
+using TechVault.Core.Interfaces;
+using TechVault.Infrastructure.Data;
+
+namespace TechVault.Infrastructure.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>,ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext _dbContext):base(_dbContext)
+        {
+            
+        }
+    }
+}
