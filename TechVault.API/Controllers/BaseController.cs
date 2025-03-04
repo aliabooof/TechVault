b00c72256f323b235467f1848d431996073fb35b
Ali Abooof <aliabooof@outlook.com>
2025-03-04 15:38:17 +0200

    get all categories endpoint

---
+++ TechVault.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechVault.Core.Interfaces;
+
+namespace TechVault.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+
+        public BaseController(IUnitOfWork unitOfWork)
+        {
+           _unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ TechVault.API/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechVault.Core.Interfaces;
+
+namespace TechVault.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IUnitOfWork unitOfWork):base(unitOfWork)
+        {
+            
+        }
+
+        [HttpGet("Get-all")]
+        public async Task<IActionResult> get()
+        {
+            try { 
+                var category = await _unitOfWork.CategoryRepository.GetAllAsync();
+
+                if (category == null) 
+                {
+                    return BadRequest();
+                }
+                return Ok(category);
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
